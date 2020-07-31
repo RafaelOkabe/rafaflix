7cd8de1f6d179c33277724b6cd1e65f75fb265e5
RafaelOkabe <rafa.okabe@gmail.com>
2020-07-31 15:57:17 -0300

    gg

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias'
+    const URL_TOP = 'https://rafinhaflix.herokuapp.com/categorias'
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json()
@@ -38,104 +38,69 @@ function CadastroCategoria() {
           ...resposta,
         ])
       })
+  }, [])
 
-    // setTimeout(() => {
-    //   setCategorias([
-    //     ...categorias,
-    //     {
-    //       "id": 1,
-    //       "nome": "Análise de Dados",
-    //       "descricao": "Vídeos sobre análise e modelagem de Dados",
-    //       "cor": "#00C86F"
-    //     },
-    //     {
-    //       "id": 2,
-    //       "nome": "Desenvolvimento Web",
-    //       "descricao": "Tudo o que você precisa saber para o desenvolvimento Web",
-    //       "cor": "#6BD1FF"
-    //     },
-    //     {
-    //       "id": 3,
-    //       "nome": "Hardware",
-    //       "descricao": "Para quem é fanático por Hardwares",
-    //       "cor": "#9cd33b"
-    //     },
-    //     {
-    //       "id": 4,
-    //       "nome": "Linux",
-    //       "descricao": "Para quem gosta de se aventurar com outros Sistemas Operacionais",
-    //       "cor": "orange"
-    //     },
-    //     {
-    //       "id": 5,
-    //       "nome": "Aprendendo a aprender tecnologia",
-    //       "descricao": "Descubra como aprender de fato a programar",
-    //       "cor": "#6b5be2"
-    //     }
-    //   ])
-    })
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-    return (
-      <PageDefault>
-        <h1>Cadastro de Categoria: {values.nome}</h1>
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault()
+        setCategorias([
+          ...categorias,
+          values
+        ])
 
-        <form onSubmit={function handleSubmit(infosDoEvento) {
-          infosDoEvento.preventDefault()
-          setCategorias([
-            ...categorias,
-            values
-          ])
+        setValues(valoresIniciais)
+      }}>
 
-          setValues(valoresIniciais)
-        }}>
+        <FormField
+          label='Nome da Categoria'
+          type='text'
+          name='nome'
+          value={values.nome}
+          onChange={handlerChange}
+        />
 
-          <FormField
-            label='Nome da Categoria'
-            type='text'
-            name='nome'
-            value={values.nome}
-            onChange={handlerChange}
-          />
-
-          <FormField
-            label='Descrição'
-            type='textarea'
-            name='descricao'
-            value={values.descricao}
-            onChange={handlerChange}
-          />
-          <FormField
-            label='Cor'
-            type='color'
-            name='cor'
-            value={values.cor}
-            onChange={handlerChange}
-          />
-          <Button>
-            Cadastrar
+        <FormField
+          label='Descrição'
+          type='textarea'
+          name='descricao'
+          value={values.descricao}
+          onChange={handlerChange}
+        />
+        <FormField
+          label='Cor'
+          type='color'
+          name='cor'
+          value={values.cor}
+          onChange={handlerChange}
+        />
+        <Button>
+          Cadastrar
             </Button>
-        </form>
+      </form>
 
-        {categorias.length === 0 && (
-          <div>
-            Loading...
-          </div>
-        )}
-        <ul>
-          {categorias.map((categorias, indice) => {
-            return (
-              <li key={`${categorias.nome}${indice}`}>
-                {categorias.nome}
-              </li>
-            )
-          })}
-        </ul>
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
+      <ul>
+        {categorias.map((categorias, indice) => {
+          return (
+            <li key={`${categorias.nome}${indice}`}>
+              {categorias.nome}
+            </li>
+          )
+        })}
+      </ul>
 
-        <Link to='/'>
-          Ir para a Home
+      <Link to='/'>
+        Ir para a Home
         </Link>
-      </PageDefault>
-    )
-  }
+    </PageDefault>
+  )
+}
 
 export default CadastroCategoria
